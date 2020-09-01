67eed0ef8fe6353492145cad19d2c2f5f630e852
dev2ajsmsig <dwibonggo25@gmail.com>
2020-09-01 10:59:28 +0700

    Add data in button fragment

--- app/src/main/java/com/example/simpleappswithhilt/ButtonsFragment.kt
+++ app/src/main/java/com/example/simpleappswithhilt/ButtonsFragment.kt
@@ -1,7 +1,29 @@
 package com.example.simpleappswithhilt
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.simpleappswithhilt.data.LoggerDataSource
+import com.example.simpleappswithhilt.di.InMemoryLogger
+import com.example.simpleappswithhilt.navigator.AppNavigator
+import javax.inject.Inject
 
 class ButtonsFragment : Fragment() {
 
+    @InMemoryLogger
+    @Inject
+    lateinit var logger: LoggerDataSource
+
+    @Inject
+    lateinit var navigator: AppNavigator
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_buttons, container, false)
+    }
 }
